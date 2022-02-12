6dd13f9af3d7d2d89ebfa260dca055eee61e6600
Gink3 <telltaylor13@gmail.com>
2022-02-12 11:23:16 -0600

    Added Berry bush function

--- src/sim/plant.rs
+++ src/sim/plant.rs
@@ -5,9 +5,31 @@ pub struct Plant {
     species: String,
     edible: bool,
     servings: usize,
-
 }
 
 impl Plant {
+    pub fn berry_bush() -> Plant {
+        Plant {
+            species: "Berry Bush".to_string(),
+            edible: true,
+            servings: 4,
+        }
+    }
 
+    pub fn get_species(self) -> String {
+        self.species
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_berry_bush() {
+        let bb = Plant::berry_bush();
+
+        assert_eq!(bb.get_species(), "Berry Bush");
+    }
 }
