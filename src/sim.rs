05c78a52f1e9efbae84f881103c6cdcc35bf7746
Taylor King <43785015+Gink3@users.noreply.github.com>
2022-03-27 10:20:09 -0500

    Moved snap shot interval to increase readability

--- src/sim.rs
+++ src/sim.rs
@@ -223,9 +223,12 @@ impl Sim {
     // Plants get 1
     pub fn run(&self, days: usize)
     {
+        let snap_interval: usize = 100;
         for d in 0..days
         {
-            if d % 100 == 0
+
+            // every 100 days take a snapshot
+            if d % snap_interval == 0
             {
                 self.snapshot(d);
             }
